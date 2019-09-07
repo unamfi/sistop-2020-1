f48f88a2980d84cb3df27d94131850d619d1c4e8
Andrés González <gandresto@outlook.com>
2019-09-07 03:16:03 -0500

    Ahora con argumentos pasados por terminal.

--- tareas/1/GonzálezAndrés/main.py
+++ tareas/1/GonzálezAndrés/main.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from threading import Semaphore, Condition
-from time import sleep
-from random import randint
-from cubiculo import Alumno, Profesor, Cubiculo
+import sys, getopt
 from faker import Faker
+from cubiculo import Alumno, Profesor, Cubiculo
 
-def main():
+def main(argv):
     fake = Faker()
-
     num_alumnos = 5
     num_sillas = 2
 
-    sem_entrada = Semaphore(num_sillas)
-    
-    prof = Profesor(fake.name(), sem_entrada) 
-    Cubiculo(sillas=num_sillas, nombre_profesor=fake.name(), num_alumnos=num_alumnos).start()
+    try:
+        opts, args = getopt.getopt(argv,"hs:a:",["n_sillas=","n_alumnos="])
+    except getopt.GetoptError:
+        print("uso: main.py -s <num_sillas> -a <num_alumnos>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print("uso: main.py -s <num_sillas> -a <num_alumnos>")
+            sys.exit()
+        elif opt in ("-s", "--n_sillas"):
+            num_sillas = int(arg)
+        elif opt in ("-a", "--n_alumnos"):
+            num_alumnos = int(arg)
+
+    Cubiculo(sillas=num_sillas, num_alumnos=num_alumnos, nombre_profesor=fake.name()).start()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
