509efa1a832165cad99c6f169c4754c2b940b154
Andrés González <gandresto@outlook.com>
2019-09-07 04:10:17 -0500

    Preguntas más aleatorias y quitar preguntas_prof.

--- tareas/1/GonzálezAndrés/cubiculo.py
+++ tareas/1/GonzálezAndrés/cubiculo.py
@@ -6,31 +6,35 @@ from faker import Faker
 
 class Alumno(Thread):
     #def __init__(self, name, cond_entrada, cond_pregunta, cond_resp, preguntas=[]):
-    def __init__(self, name, preguntas_alumno=[], preguntas_profesor=[], 
+    def __init__(self, name, alumnos_dentro, num_max_preguntas=5, 
                     sem_entrada = None, mutex_pregunta = None, preg_lista=None, resp_lista=None):
         Thread.__init__(self)
         self.name = name
-        self.preguntas_alumno = preguntas_alumno
-        self.preguntas_profesor = preguntas_profesor
+        self.num_max_preguntas = num_max_preguntas
         self.sem_entrada = sem_entrada
         self.mutex_pregunta = mutex_pregunta
         self.preg_lista = preg_lista
         self.resp_lista = resp_lista
+        self.alumnos_dentro = alumnos_dentro
+
+        self.preguntas_alumno = ['preg'+str(i) for i in range(randint(1,num_max_preguntas))]
 
     def preguntar(self):
         pregunta = self.preguntas_alumno.pop()
         with self.mutex_pregunta:
             print("\t%s hace una pregunta: %s" % (self.name, pregunta))
-            sleep(0.5)
+            sleep(0.1)
             self.preg_lista.release()
             print("\t%s está esperando respuesta..." % (self.name))
             self.resp_lista.acquire()
 
     def preguntar_todo(self):
         num_preguntas = len(self.preguntas_alumno)
-        print("Hola, soy %s y tengo %i pregunta(s)" % (self.name, num_preguntas))
+        print("Hola, soy %s y quiero entrar al cubículo." % (self.name))
         with self.sem_entrada:
-            print("<-- %s entró al cubículo" % self.name)
+            self.alumnos_dentro.append(self.name)
+            print(("--> %s entró al cubículo y tiene %i preguntas. "+
+                    "\n    Somos %i personas en el cubiculo") % (self.name, num_preguntas ,len(self.alumnos_dentro)))
             t_list = []
             for _ in range(num_preguntas):
                 t = Thread(target=self.preguntar)
@@ -38,18 +42,18 @@ class Alumno(Thread):
                 t.start()
             for t in t_list:
                 t.join()
-            print("--> %s salió del cubículo" % self.name)
+            self.alumnos_dentro.remove(self.name)
+            print("<-- %s salió del cubículo. Quedan %i personas en el cubiculo" % (self.name, len(self.alumnos_dentro)))
 
     def run(self):    
         Thread(target=self.preguntar_todo).start()
         
 
 class Profesor(Thread):
-    def __init__(self, name='Fulano', preguntas_profesor=[], sem_entrada = None, 
+    def __init__(self, name='Dr. Fulano', sem_entrada = None, 
                     mutex_pregunta = None, preg_lista=None, resp_lista=None):
         Thread.__init__(self)
         self.name = name
-        self.preguntas_profesor = preguntas_profesor
         self.sem_entrada = sem_entrada
         self.mutex_pregunta = mutex_pregunta
         self.preg_lista = preg_lista
@@ -59,7 +63,7 @@ class Profesor(Thread):
         print("\tEl profesor está esperando una pregunta...")
         self.preg_lista.acquire()
         print("\tEl profesor está respondiendo una pregunta...")
-        sleep(0.5)
+        sleep(0.1)
         self.resp_lista.release()
 
 
@@ -78,6 +82,7 @@ class Cubiculo(Thread):
         self.sillas = sillas
         self.num_alumnos = num_alumnos
         self.nombre_profesor = nombre_profesor
+        self.alumnos_dentro = []
 
         self.sem_entrada = Semaphore(sillas)
         self.mutex_pregunta = Semaphore(1)
@@ -85,17 +90,16 @@ class Cubiculo(Thread):
         self.resp_lista = Semaphore(0)
 
         fake = Faker()
-        #preguntas_alumno = ['preg'+str(i) for i in range(randint(1,6))]
+        #Crear N numero de alumnos
         self.alumnos = [Alumno(name=fake.name(), 
-                            preguntas_alumno=['preg'+str(i) for j in range(randint(1,6))], 
-                            preguntas_profesor=[], 
-                            sem_entrada = self.sem_entrada, 
-                            mutex_pregunta = self.mutex_pregunta, 
-                            preg_lista=self.preg_lista, 
-                            resp_lista=self.resp_lista)
+                                alumnos_dentro = self.alumnos_dentro,
+                                num_max_preguntas=6, 
+                                sem_entrada = self.sem_entrada, 
+                                mutex_pregunta = self.mutex_pregunta, 
+                                preg_lista=self.preg_lista, 
+                                resp_lista=self.resp_lista)
                         for i in range(num_alumnos)]   
         self.profesor = Profesor(name=fake.name(), 
-                                preguntas_profesor=[], 
                                 sem_entrada = self.sem_entrada, 
                                 mutex_pregunta = self.mutex_pregunta, 
                                 preg_lista=self.preg_lista, 
@@ -112,3 +116,4 @@ class Cubiculo(Thread):
         print("Iniciando cubículo con %i sillas..." % self.sillas)
         t = Thread(target=self.iniciar_operaciones)
         t.run()
+        #exit(0)
