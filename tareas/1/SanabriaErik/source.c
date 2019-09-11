6d31300f927524ef2e37baa439f3198e49077b99
Erik Sanabria <esanabria136@gmail.com>
2019-09-11 20:47:17 +0000

    Definiendo los datos requeridos

--- tareas/1/SanabriaErik/source.c
+++ tareas/1/SanabriaErik/source.c
@@ -2,32 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <pthread.h>
 
-size_t num_rand()
+typedef struct
 {
-	const size_t a = 10;	//minimo
-	const size_t b = 50;	//maximo
+	size_t n;
+	bool en_uso;
 	
+}Plato;
+
+size_t num_rand(size_t a, size_t b)
+{	
 	return ((rand() % (b - a + 1)) - 1);
 }
 
-void comer()
+void *comer()
 {
-	const size_t tiempo = 6;
+	const size_t tiempo = num_rand(2, 5);
 	
 	printf("\n\tComiendo... \n\tAhora durmiendo por %zu segundos.", tiempo);
 	sleep(tiempo);
 	
 }
 
-void gato_come()
+void *gato_come(pthread_t *n)
 {
 	//empiezo a comer
 	//si veo raton me lo como primero
 	//si no hay ratones voy directo a comer
 }
 
-void raton_come()
+void *raton_come(pthread_t *n)
 {
 	//si no hay gatos como
 	//si si huyo
@@ -36,11 +41,32 @@ void raton_come()
 int main(void)
 {
 	srand(time(0));
+	pthread_t id;
+	pthread_t id_2;
 	
-	size_t gatos = num_rand();
-	size_t raton = num_rand();
-	size_t platos = num_rand(); //necesitan mutex
+	size_t gatos = num_rand(10, 40);
+	size_t raton = num_rand(10, 40);
+	size_t platos = num_rand(10, 40); //necesitan mutex
 	
+	size_t *a_gatos = (size_t *)calloc(gatos, sizeof(gatos));
+	size_t *a_ratones = (size_t *)calloc(raton, sizeof(raton));
+	size_t *a_platos = (size_t *)calloc(Plato, sizeof(Plato));
+	
+	for(size_t i = 0; i < gatos; ++i)
+	{
+		pthread_create(&id, NULL, gato_come, (void *)&id);
+	}
+	
+	for(size_t j = 0; j < raton; ++j)
+	{
+		pthread_create(&id_2, NULL, raton_come, (void *)&id);
+	}
+	
+	free(a_gatos);
+	free(a_ratones);
+	free(a_platos);
+	
+	pthread_exit(NULL);
 	printf("\n\n");
 	
 	return 0;
