59df58f701f4f67be1d61ce23fbc3d873d3b26b8
Erik Sanabria <esanabria136@gmail.com>
2019-09-12 00:10:27 +0000

    limpiando comentarios

--- tareas/1/SanabriaErik/source.c
+++ tareas/1/SanabriaErik/source.c
@@ -40,33 +40,22 @@ void *comer(pthread_t* m)
 	const size_t tiempo = num_rand(2, 5);
 	
 	printf("\n\tComiendo... \n\tAhora durmiendo por %zu segundos.", tiempo);
-	fflush(stdout);
 	sleep(tiempo);
 	
 }
 
-void *gato_come(Argv* argv/*pthread_t* n, size_t num, void(*comer)(pthread_t* m)*/)
+void *gato_come(Argv* argv)
 {
 	size_t num = argv->m_num;
 	pthread_t n = argv->m_id;
 	printf("\n\tGato %zu comiendo en Thread ID: %d", num, n);
-	fflush(stdout);
-	
-	//comer(n);
-	//empiezo a comer
-	//si veo raton me lo como y sigo comiendo
-	//si no hay ratones voy directo a comer
 }
 
-void *raton_come(Argv* argv/*pthread_t* n, size_t num, void(*comer)(pthread_t* m)*/)
+void *raton_come(Argv* argv)
 {
 	size_t num = argv->m_num;
 	pthread_t n = argv->m_id;	
 	printf("\n\tRaton %zu comiendo en Thread ID: %d", num, n);
-	fflush(stdout);
-	//comer(n);
-	//si no hay gatos como
-	//si si huyo
 }
 
 int main(void)
@@ -76,31 +65,31 @@ int main(void)
 	const size_t gatos = num_rand(10, 40);
 	const size_t raton = num_rand(10, 40);
 	const size_t platos = num_rand(10, 40); //necesitan mutex
+	//https://www.cs.cmu.edu/afs/cs/academic/class/15492-f07/www/pthreads.html
 	
 	Animal *a_gatos = (Animal*)calloc(gatos, sizeof(Animal));
 	Animal *a_ratones = (Animal*)calloc(raton, sizeof(Animal));
 	Plato *a_platos = (Plato*)calloc(platos, sizeof(Plato));
 	
 	printf("\n\tCreando %zu gatos, %zu ratones y %zu platos.\n", gatos, raton, platos);
-	fflush(stdout);
 	
 	for(size_t i = 0; i < gatos; ++i)
 	{
 		Argv* argum = (Argv*)calloc(1, sizeof(Argv));
 		argum->m_num = i;
-		pthread_create(&(a_gatos[i].id), NULL, gato_come, (void*)&(argum));
+		pthread_create(&(a_gatos[i].id), NULL, gato_come, (void*)&argum);
 		argum->m_id = a_gatos[i].id;
 		free(argum);
 	}
 	
-	for(size_t j = 0; j < raton; ++j)
+	/*for(size_t j = 0; j < raton; ++j)
 	{
 		Argv* argum_2 = (Argv*)calloc(1, sizeof(Argv));
 		argum_2->m_num = j;
-		pthread_create(&(a_ratones[j].id), NULL, raton_come, (void *)&(argum_2));
+		pthread_create(&(a_ratones[j].id), NULL, raton_come, (void*)&argum_2);
 		argum_2->m_id = a_ratones[j].id;
 		free(argum_2);
-	}
+	}*/
 	
 	free(a_gatos);
 	free(a_ratones);
@@ -109,7 +98,6 @@ int main(void)
 	pthread_exit(NULL);
 	
 	printf("\n\n\n");
-	fflush(stdout);
 	
 	return 0;
 }
