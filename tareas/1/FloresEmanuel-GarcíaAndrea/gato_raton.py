a160520b5e541a90c2c62342afbd035a6170b26a
Andrea García <andiee.grr@gmail.com>
2019-09-13 00:01:16 -0500

    Programa comentado

--- tareas/1/FloresEmanuel-GarcíaAndrea/gato_raton.py
+++ tareas/1/FloresEmanuel-GarcíaAndrea/gato_raton.py
@@ -1,26 +1,32 @@
 import threading
 import sys
 
+#Protege la variable ratones
 mutex = threading.Semaphore(1)
+#Protege la variable gatos
 mutexg = threading.Semaphore(1)
+#Se usa el patrón multiplex para verificar, que únicamente un animal coma a la vez de un plato, por lo que los animales comiendo a la vez en todos los platos no deben sobrepasar el número de platos que se le pasa como parámetro
 n_platos = threading.Semaphore(int(sys.argv[3]))
 ratones = 0
 gatos = 0
+#Se utiliza para checar la condición de que ningún gato puede acercarse cuando hay ratones comiendo, utiliza el patrón apagador
 puede_comer = threading.Semaphore(1)
 
 class Gato():
     def __init__(self, num_gato):
         self.num_gato = num_gato
         print("Soy el gato %d y tengo hambre" % (self.num_gato))
-        #print('En este momento hay %d hilos en ejecuci贸n.' % len(threading.enumerate()))
         self.comerPlato()
 
     def comerPlato(self):
         global gatos
+        #Una vez que se valida que no hay ratones, procedemos a comer 
         puede_comer.acquire()
+        #Permite que los ratones se acerquen a comer aunque haya gatos comiendo
         puede_comer.release()
+        #Pide un plato
         n_platos.acquire()
-        print("Soy el gato  %d y estoy comiendo" % (self.num_gato))
+        print("Soy el gato  %d y estoy comiendo" % (self.num_gato))        
         mutexg.acquire()
         gatos = gatos +1
         mutexg.release()
@@ -39,13 +45,16 @@ class Raton:
 
     def comerPlato(self):
         global ratones, gatos
+        #Pide un plato
         n_platos.acquire()
         mutex.acquire()
         ratones += 1
+        #Si es el primer ratón que llega y enciende el apagador indicando que los ratones están comiendo
         if ratones == 1:
             puede_comer.acquire()
         mutex.release()
         print("Soy el ratón %d y estoy comiendo" % (self.raton))
+        #Revisa que no haya gatos comiendo        
         mutexg.acquire()
         if(gatos > 0):
             self.vivo = False
@@ -56,6 +65,7 @@ class Raton:
             print("Se han comido al ratón %d" % (self.raton))        
         mutex.acquire()
         ratones-=1
+        #Una vez que todos los ratones se hayan ido, apaga e indica que ahora los gatos pueden comer
         if ratones == 0:
             puede_comer.release()
         mutex.release()
