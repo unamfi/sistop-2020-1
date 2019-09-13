165ed5544d091cd14fb21af23abe5f428e08c423
Andrea García <andiee.grr@gmail.com>
2019-09-12 23:34:55 -0500

    Cambios

--- tareas/1/FloresEmanuel-GarcíaAndrea/gato_raton.py
+++ tareas/1/FloresEmanuel-GarcíaAndrea/gato_raton.py
@@ -2,54 +2,60 @@ import threading
 import sys
 
 mutex = threading.Semaphore(1)
+mutexg = threading.Semaphore(1)
 n_platos = threading.Semaphore(int(sys.argv[3]))
 ratones = 0
+gatos = 0
 puede_comer = threading.Semaphore(1)
 
 class Gato():
     def __init__(self, num_gato):
         self.num_gato = num_gato
         print("Soy el gato %d y tengo hambre" % (self.num_gato))
-        #print('En este momento hay %d hilos en ejecución.' % len(threading.enumerate()))
+        #print('En este momento hay %d hilos en ejecuci贸n.' % len(threading.enumerate()))
         self.comerPlato()
 
     def comerPlato(self):
-        global ratones
+        global gatos
         puede_comer.acquire()
         puede_comer.release()
         n_platos.acquire()
-        print("Soy un gato  %d y estoy comiendo" % (self.num_gato))
-        mutex.acquire()
-        if ratones != 0:
-               print("El gato %d se comió al ratón %d" % (self.num_gato,ratones))
-               ratones -= 1
-               n_platos.release()
+        print("Soy el gato  %d y estoy comiendo" % (self.num_gato))
+        mutexg.acquire()
+        gatos = gatos +1
+        mutexg.release()
         print('El gato %d ha terminado de comer' % (self.num_gato))
-        mutex.release()
+        mutexg.acquire()
+        gatos = gatos -1
+        mutexg.release()
         n_platos.release()
 
 class Raton:
     def __init__(self, num_raton):
         self.raton = num_raton
+        self.vivo = True
         print("Soy el raton %d y tengo hambre" % (num_raton))
-        #print('En este momento hay %d hilos en ejecución.' % len(threading.enumerate()))
         self.comerPlato()
 
     def comerPlato(self):
-        global ratones
+        global ratones, gatos
         n_platos.acquire()
         mutex.acquire()
         ratones += 1
         if ratones == 1:
             puede_comer.acquire()
         mutex.release()
-        print("Soy un ratón %d y estoy comiendo" % (self.raton))
-        #n_platos.release()
-        print("Soy el ratón %d y terminé de comer" % (self.raton))
+        print("Soy el ratón %d y estoy comiendo" % (self.raton))
+        mutexg.acquire()
+        if(gatos > 0):
+            self.vivo = False
+        mutexg.release()
+        if(self.vivo == True):
+            print("Soy el ratón %d y terminé de comer" % (self.raton))                
+        else:
+            print("Se han comido al ratón %d" % (self.raton))        
         mutex.acquire()
         ratones-=1
-        #n_platos.release()
-        #print("Soy el ratón %d y terminé de comer" % (self.raton))
         if ratones == 0:
             puede_comer.release()
         mutex.release()
@@ -64,7 +70,7 @@ if __name__ == '__main__':
 
    #------------------------------------
    #Inicia los hilos
-   #Mientras no se lo indiquemos sigue creando gatos   
+   #Genera la cantidad de gatos y ratones que se le indique   
     for i in range(num_gatos):
         threading.Thread(target=Gato, args=[i]).start()
 
