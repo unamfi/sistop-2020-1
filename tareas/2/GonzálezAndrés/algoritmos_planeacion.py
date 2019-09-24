30e8b614ed47f3dc635611d8840cb082a60ff251
Andrés González <gandresto@outlook.com>
2019-09-23 21:35:19 -0500

    FCFS terminado.

--- tareas/2/GonzálezAndrés/algoritmos_planeacion.py
+++ tareas/2/GonzálezAndrés/algoritmos_planeacion.py
@@ -1,34 +1,53 @@
 import numpy as np
 
 class Proceso(object):
-    def __init__(self, nombre, t_llegada, t_requerido, t_respuesta=0, t_espera=0, p_penalizacion=0, p_respuesta=0):
+    def __init__(self, nombre, t_llegada, t_requerido, t_respuesta=0):
         self.nombre = nombre
         self.t_llegada = t_llegada
         self.t_requerido = t_requerido
         self.t_respuesta = t_respuesta
-        self.t_espera = t_espera
-        self.p_penalizacion = p_penalizacion
-        self.p_respuesta = p_respuesta
+        self.t_restante = self.t_requerido
 
+    @property
+    def t_espera(self): 
+        return self.t_respuesta - self.t_requerido
 
-def fcfs(procesos=[]):
+    @property
+    def p_penalizacion(self): 
+        return self.t_respuesta / self.t_requerido
+
+    @property
+    def p_respuesta(self): 
+        return self.t_requerido / self.t_respuesta
+
+def fcfs(lista_procesos=[]):
     """First come, first serve.
     Ingresa una lista de procesos ordenados por tiempo de llegada y devuelve la cola de ejecución.
     También devuelve tiempo de respuesta (T), tiempo de espera (E), proporción de penalización (P) y proporción de respuesta (R). 
     """
-    T=0
-    E=0
-    P= 0
-    R=0
 
     t_total = 0
     cola_ejec = []
+    procesos_ejecutados = []
 
-    while procesos:
-        np.append(cola_ejec, procesos[0])
-        procesos = procesos[1:]
-        for i, proceso in enumerate(procesos):            
-            proceso
-        t_total += 1
+    while lista_procesos:
+        if lista_procesos[0].t_llegada <= t_total:
+            proceso_en_ejec = lista_procesos[0]
+            lista_procesos = lista_procesos[1:]
+            
+            while proceso_en_ejec.t_restante:
+                cola_ejec.append(proceso_en_ejec.nombre)     
+                proceso_en_ejec.t_restante -= 1
+                proceso_en_ejec.t_respuesta += 1
+                for i in range(len(lista_procesos)):
+                    if lista_procesos[i].t_llegada < t_total:
+                        lista_procesos[i].t_requerido += 1
+                    else:
+                        break
+                t_total+=1        
 
-    return [procesos, T, E, P, R]
+            procesos_ejecutados.append(proceso_en_ejec)
+        else:
+            t_total += 1
+
+    return [cola_ejec, procesos_ejecutados]
--- tareas/2/GonzálezAndrés/tarea2.py
+++ tareas/2/GonzálezAndrés/tarea2.py
@@ -6,16 +6,20 @@ from algoritmos_planeacion import fcfs, Proceso
 #%%
 nombre_procesos = np.array(list(string.ascii_uppercase))
 tiempos_llegadas = np.sort(np.random.randint(low='0', high='50', size=26))
-tiempos_requeridos = np.random.randint(low='0', high='8', size=26)
+tiempos_requeridos = np.random.randint(low='1', high='8', size=26)
 
 lista_procesos = []
 
-for i in range(26):
+num_procesos = 7
+
+for i in range(num_procesos):
     nombre = nombre_procesos[i]
     tiempo_llegada = tiempos_llegadas[i]
     tiempo_requerido = tiempos_requeridos[i]
     lista_procesos.append(Proceso(nombre, tiempo_llegada, tiempo_requerido))
+    print("%s: t_llegada=%d, t_requerido=%d." % (nombre, tiempo_llegada, tiempo_requerido))
 #%%
-#[lista_ejecucion, T, E, P, R] = fcfs(lista_procesos[:7])
+[cola_ejecucion, procesos_ejecutados] = fcfs(lista_procesos)
+print('FCFS: ', ''.join(cola_ejecucion))
 
 #%%
