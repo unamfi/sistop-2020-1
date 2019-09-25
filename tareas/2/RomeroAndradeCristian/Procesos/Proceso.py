f9d65c80be9cdf4c71712cf57eb05a60c40f6527
Cristian Romero <mascrit@gmail.com>
2019-09-24 21:18:52 -0500

    Estructura de procesos y plan

---
+++ tareas/2/RomeroAndradeCristian/Procesos/Proceso.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- codig: utf-8 -*-
+
+from string import ascii_uppercase
+from random import choice, randint
+
+class Proceso:
+
+    def __init__(self, tick_lim: list, llegada_lim: list):
+        self.color_names = ['\033[31m','\033[32m','\033[33m','\033[34m',
+                            '\033[36m','\033[37m','\033[91m','\033[92m',
+                            '\033[93m','\033[94m','\033[95m']
+        self.color = self.color_names[randint(0,len(self.color_names)-1)]
+        self.nombre = self.color + "[" + str(choice(ascii_uppercase)) + "]\033[0m"
+        self.t = randint(tick_lim[0], tick_lim[1])
+        self.llegada = randint(llegada_lim[0], llegada_lim[1])
---
+++ tareas/2/RomeroAndradeCristian/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# -*-coding: utf-8 -*-x
+
+A, B, C = Proceso(3), Proceso(5, 1), Proceso(2, 3)
+D, E = Proceso(5, 9), Proceso(5, 12)
