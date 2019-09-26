cfd3a2a8c80e109c11c0edbe1efe32a4a524f017
Andrés González <gandresto@outlook.com>
2019-09-26 02:34:10 -0500

    Ahora se pueden personalizar los t máx requeridos y de llegada de los procesos

--- tareas/2/GonzálezAndrés/tarea2.py
+++ tareas/2/GonzálezAndrés/tarea2.py
@@ -8,22 +8,36 @@ import algoritmos_planeacion as ap
 def main(argv):
 
     num_procesos = 5
+    llegada_max = 10
+    requerido_max = 10
     seed = None
 
     try:
-        opts, args = getopt.getopt(argv,"hp:s:",["procesos=","seed="])
+        opts, args = getopt.getopt(argv,"hp:l:t:s:",["procesos=","llegada_max=","requerido_max=","semilla="])
     except getopt.GetoptError:
-        print("uso: tarea2.py -p <num de procesos> -s <seed>")
+        print("uso: tarea2.py -p <num de procesos> -l <t de llegada máx.> -t <t requerido máx.> -s <semilla>")
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print("uso: tarea2.py -p <num de procesos> -s <seed>")
-            print("El número de procesos debe ser entre 1 y 26")
+            print("uso: tarea2.py -p <num de procesos> -l <t de llegada máx.> -t <t requerido máx.> -s <semilla>")
+            print("El número de procesos debe estar entre 1 y 26")
+            print("El tiempo máximo de llegada debe ser un número positivo")
+            print("El tiempo requerido máximo debe ser un número mayor a 0")
             sys.exit()
         elif opt in ("-p", "--procesos"):
             num_procesos = int(arg)
             if num_procesos<=0 or num_procesos>26:
                 print("El número de procesos debe ser entre 1 y 26")
+                sys.exit(2)
+        elif opt in ("-l", "--tiempo_llegada"):
+            llegada_max = int(arg)
+            if llegada_max<=1:
+                print("El tiempo máximo de llegada debe ser un número positivo")
+                sys.exit(2)
+        elif opt in ("-t", "--tiempo_requerido"):
+            requerido_max = int(arg)
+            if requerido_max<=1:
+                print("El tiempo máximo requerido debe ser un número mayor a 0")
                 sys.exit(2)
         elif opt in ("-s", "--seed"):
             seed = int(arg)
@@ -31,8 +45,8 @@ def main(argv):
     np.random.seed(seed)
 
     nombre_procesos = np.array(list(string.ascii_uppercase))
-    tiempos_llegadas = np.sort(np.random.randint(low='0', high='35', size=26))
-    tiempos_requeridos = np.random.randint(low='1', high='10', size=26)
+    tiempos_llegadas = np.sort(np.random.randint(low='0', high=llegada_max, size=num_procesos))
+    tiempos_requeridos = np.random.randint(low='1', high=requerido_max, size=num_procesos)
 
     lista_procesos = []
 
