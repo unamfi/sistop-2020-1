574a14e2dc52cdba89f358cd8a8d1ca4f09a6ce0
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-09-25 20:59:33 -0500

    Modified main.py to adopt rr4.py

--- tareas/2/MartinezNestor/main.py
+++ tareas/2/MartinezNestor/main.py
@@ -1,12 +1,18 @@
 from foundations import * 
 from rr1 import *
+from rr4 import *
+from fcfs import *
 
 #Main method
 f = Foundation()
 
 def main():
 	procs = generateTest()
+	#Round Robin with quantum = 1
 	rr1(procs)
+	#Round Robin with quantum = 4
+	rr4(procs,4)
+
 
 #generateTest() generates a hard coded data set of processes
 def generateTest():
