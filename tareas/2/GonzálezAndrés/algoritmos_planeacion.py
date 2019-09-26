9777ce9926105f8151b21ab54d37ca0f37264b34
Andrés González <gandresto@outlook.com>
2019-09-25 22:59:29 -0500

    RoundRobin funcionando :D

--- tareas/2/GonzálezAndrés/algoritmos_planeacion.py
+++ tareas/2/GonzálezAndrés/algoritmos_planeacion.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 class Proceso(object):
-    def __init__(self, nombre, t_llegada, t_requerido, t_respuesta=0):
+    def __init__(self, nombre : str, t_llegada : int, t_requerido : int):
         self.nombre = nombre
         self.t_llegada = t_llegada
         self.t_requerido = t_requerido
-        self.t_respuesta = t_respuesta
-        self.t_restante = self.t_requerido
+        self.t_restante = t_requerido
+        self.t_finalizacion = t_llegada + t_requerido
+
+    @property
+    def t_respuesta(self): 
+        return self.t_finalizacion - self.t_llegada
 
     @property
     def t_espera(self): 
@@ -19,48 +23,71 @@ class Proceso(object):
     @property
     def p_respuesta(self): 
         return self.t_requerido / self.t_respuesta
+    
+    def reset(self):
+        self.t_restante = self.t_requerido
+        self.t_finalizacion = self.t_llegada + self.t_requerido
 
 def fcfs(lista_procesos=[]):
     """First come, first serve.
     Ingresa una lista de procesos ordenados por tiempo de llegada y devuelve la cola de ejecución.
-    También devuelve la lista de procesos de entrada con los tiempos de ejecución. 
-    """
+    También devuelve la lista de procesos de entrada con los tiempos de ejecución.
 
+    """
     t_total = 0
     cola_ejec = []
     procesos_ejecutados = []
+    lp = lista_procesos
 
-    while lista_procesos:
-        if lista_procesos[0].t_llegada <= t_total:
-            proceso_en_ejec = lista_procesos[0]
-            lista_procesos = lista_procesos[1:]
-            
-            while proceso_en_ejec.t_restante:
-                cola_ejec.append(proceso_en_ejec.nombre)     
-                proceso_en_ejec.t_restante -= 1
-                proceso_en_ejec.t_respuesta += 1
-                for i in range(len(lista_procesos)):
-                    if lista_procesos[i].t_llegada <= t_total:
-                        lista_procesos[i].t_respuesta += 1
-                    else:
-                        break
-                t_total+=1        
+    while lp:
+        if lp[0].t_llegada <= t_total:
+            p_ejec = lp[0] # p_ejec : proceso en ejecucion
+            lp = lp[1:]
 
-            procesos_ejecutados.append(proceso_en_ejec)
+            while p_ejec.t_restante:
+                cola_ejec.append(p_ejec.nombre)     
+                p_ejec.t_restante -= 1
+                t_total+=1
+
+            p_ejec.t_finalizacion = t_total
+            procesos_ejecutados.append(p_ejec)
         else:
             t_total += 1
 
     return [cola_ejec, procesos_ejecutados]
 
 def roundrobin(lista_procesos=[], quantum=1):
-    t_total = 0
+    #t_total = 0
     cola_ejec = []
     procesos_ejecutados = []
-    while lista_procesos:
-        if lista_procesos[0].t_llegada <= t_total:
-            pass
-        else:
-            t_total+=1
+    ronda_procesos = []
+    lp = lista_procesos 
+    t_total = lp[0].t_llegada # Hacemos que inicie desde el proceso que llegó primero
+    ronda_procesos.insert(0, lp[0]) # Agregamos el primer proceso a la ronda
+    lp = lp[1:] # Removemos el primer valor de la lista
+    while lp or ronda_procesos: # Seguimos ejecutando mientras haya procesos, ya sea en ronda o aún sin iniciar
+        # p_ejec : proceso en ejecución
+        p_ejec = ronda_procesos[0] # Ejecutamos el siguiente proceso en cola para ronda
+        trq = quantum # Reiniciamos el contador para el quantum
+        while True:
+            while lp: # Validamos si algún proceso llegó mientras estabamos ejecutando otro
+                if lp[0].t_llegada <= t_total:
+                    ronda_procesos.insert(1, lp[0]) # De ser así, lo ponemos en cola de espera en la segunda posición del arreglo
+                    lp = lp[1:]
+                else:
+                    break
+            cola_ejec.append(p_ejec.nombre)
+            p_ejec.t_restante -= 1
+            trq -= 1
+            t_total += 1
+            if not p_ejec.t_restante: # Si ya ecabamos de ejecutarlo
+                p_ejec.t_finalizacion = t_total
+                procesos_ejecutados.append(p_ejec) # Lo mandamos a la lista de procesos ejecutados
+                break 
+            if not trq: # Si se acaba el tiempo en el quantum
+                ronda_procesos.append(p_ejec) # Mandamos el proceso de nuevo a la ronda
+                break
+        ronda_procesos = ronda_procesos[1:] # Removemos de la lista el proceso que acabamos de ejecutar
     return [cola_ejec, procesos_ejecutados]
 
 def fb(lista_procesos=[],n_colas_prioridad=2, ejec_p_degrad=1, quantum=1):
--- tareas/2/GonzálezAndrés/tarea2.py
+++ tareas/2/GonzálezAndrés/tarea2.py
@@ -1,26 +1,27 @@
 #%%
 import numpy as np
 import string
-from algoritmos_planeacion import fcfs, Proceso
+import algoritmos_planeacion as ap
 
 def main():
-    #%%
+    np.random.seed(100)
     nombre_procesos = np.array(list(string.ascii_uppercase))
     tiempos_llegadas = np.sort(np.random.randint(low='0', high='50', size=26))
     tiempos_requeridos = np.random.randint(low='1', high='8', size=26)
 
     lista_procesos = []
 
-    num_procesos = 7
+    num_procesos = 4
 
     for i in range(num_procesos):
         nombre = nombre_procesos[i]
         tiempo_llegada = tiempos_llegadas[i]
         tiempo_requerido = tiempos_requeridos[i]
-        lista_procesos.append(Proceso(nombre, tiempo_llegada, tiempo_requerido))
+        lista_procesos.append(ap.Proceso(nombre, tiempo_llegada, tiempo_requerido))
         print("%s: t_llegada=%d, t_requerido=%d." % (nombre, tiempo_llegada, tiempo_requerido))
-    #%%
-    [cola_ejecucion, procesos_ejecutados] = fcfs(lista_procesos)
+
+    # FIFO
+    [cola_ejecucion, procesos_ejecutados] = ap.fcfs(lista_procesos)
 
     T = [proceso.t_respuesta for proceso in procesos_ejecutados]
     T = np.mean(T)
@@ -30,6 +31,36 @@ def main():
     P = np.mean(P)
 
     print('\nFCFS: T=%.2f, E=%.2f, P=%.2f' % (T, E, P))
+    print(''.join(cola_ejecucion))
+    
+    # Round Robin 1
+    for p in lista_procesos:
+        p.reset() # Reiniciamos los procesos a sus valores iniciales
+    [cola_ejecucion, procesos_ejecutados] = ap.roundrobin(lista_procesos, 1)
+
+    T = [proceso.t_respuesta for proceso in procesos_ejecutados]
+    T = np.mean(T)
+    E = [proceso.t_espera for proceso in procesos_ejecutados]
+    E = np.mean(E)
+    P = [proceso.p_penalizacion for proceso in procesos_ejecutados]
+    P = np.mean(P)
+
+    print('\nRR1: T=%.2f, E=%.2f, P=%.2f' % (T, E, P))
+    print(''.join(cola_ejecucion))
+
+    # Round Robin 4
+    for p in lista_procesos:
+        p.reset() # Reiniciamos los procesos a sus valores iniciales
+    [cola_ejecucion, procesos_ejecutados] = ap.roundrobin(lista_procesos, 4)
+
+    T = [proceso.t_respuesta for proceso in procesos_ejecutados]
+    T = np.mean(T)
+    E = [proceso.t_espera for proceso in procesos_ejecutados]
+    E = np.mean(E)
+    P = [proceso.p_penalizacion for proceso in procesos_ejecutados]
+    P = np.mean(P)
+
+    print('\nRR4: T=%.2f, E=%.2f, P=%.2f' % (T, E, P))
     print(''.join(cola_ejecucion))
 
 if __name__ == "__main__":
