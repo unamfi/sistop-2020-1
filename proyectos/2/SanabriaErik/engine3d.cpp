12ad6f2b57316d87aee204cfbb21f8aeebd44450
Erik <esanabria136@gmail.com>
2019-09-28 14:08:56 -0500

    agregando clase hija

---
+++ proyectos/2/SanabriaErik/engine3d.cpp
@@ -0,0 +1,9 @@
+/*
+ * engine3d.cpp
+ *
+ *  Created on: Sep 28, 2019
+ *      Author: Erik Sanabria
+ */
+
+#include "engine3d.hpp"
+
---
+++ proyectos/2/SanabriaErik/engine3d.hpp
@@ -0,0 +1,24 @@
+/*
+ * engine3d.hpp
+ *
+ *  Created on: Sep 28, 2019
+ *      Author: Erik Sanabria
+ */
+
+#ifndef ENGINE3D_HPP_
+#define ENGINE3D_HPP_
+
+#include "consolegameengine.hpp"
+#include <vector>
+
+class Engine3D : public ConsoleGameEngine
+{
+public:
+	Engine3D();
+
+
+private:
+
+};
+
+#endif /* ENGINE3D_HPP_ */
