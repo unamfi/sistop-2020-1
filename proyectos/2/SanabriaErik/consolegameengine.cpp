8654d60ad1566bd84590415e904b3abc25ac15a4
Erik <esanabria136@gmail.com>
2019-09-28 09:18:12 -0500

    implementando CreaConsola

--- proyectos/2/SanabriaErik/consolegameengine.cpp
+++ proyectos/2/SanabriaErik/consolegameengine.cpp
@@ -17,6 +17,18 @@ bool ConsoleGameEngine::CreaConsola(size_t w, size_t h, size_t fonh, size_t fonw
 {
 	m_sw = w;
 	m_sh = h;
+	m_fw = fonw;
+	m_fh = fonh;
+
+	//alocando memoria para el buffer de pantalla
+	m_buffer[0] = new CHAR_INFO[m_sw * m_sh];
+	m_buffer[1] = new CHAR_INFO[m_sw * m_sh];
+
+	//llenando con ceros al buffer
+	memset(m_buffer[0], 0, m_sw * m_sh * sizeof(CHAR_INFO));
+	memset(m_buffer[1], 0, m_sw * m_sh * sizeof(CHAR_INFO));
+
+	m_buffactual = 0;
 
 	return true;
 }
