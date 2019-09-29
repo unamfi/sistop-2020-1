1140f897661f642fb3ec4ac99c2ee8b91cf25fc0
Erik <esanabria136@gmail.com>
2019-09-29 12:31:21 -0500

    agregando objetos

--- proyectos/2/SanabriaErik/main.cpp
+++ proyectos/2/SanabriaErik/main.cpp
@@ -5,10 +5,17 @@
  *      Author: Erik Sanabria
  */
 
-//#include "engine3d.hpp"
+#include "engine3d.hpp"
 
 int main()
 {
+	Engine3D A;
+
+	if(A.CreaConsola(200, 150, 4, 4))
+	{
+		A.exec();
+	}
+
 	return 0;
 }
 
