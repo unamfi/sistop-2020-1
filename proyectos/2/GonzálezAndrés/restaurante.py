2c149c3c69d0ce5cb045c718c043f6baf3fd8c8e
Andrés González <gandresto@outlook.com>
2019-09-29 17:41:26 -0500

    Agrego candados y variables globales

--- proyectos/2/GonzálezAndrés/restaurante.py
+++ proyectos/2/GonzálezAndrés/restaurante.py
@@ -2,26 +2,57 @@
 import threading as th
 
 menu = []
+clientes = 0
+ordenesListas = []
+ordenesPendientes = []
 
-class Cocinero(object):
+# Candados necesarios para variabes globales
+lClientes = th.Lock()
+lMenu = th.Lock()
+lOrdenesListas = th.Lock()
+lOrdenesPendiente = th.Lock()
+
+class Mesero(object):
+    def __init__(self, nombre : str):
+        pass
+
     def recibirOrden(self, parameter_list):
         pass
+
     def anotarOrden(self, parameter_list):
         pass
+
     def obtenerPlatillo(self):
         pass
+
     def entregarPlatillo(self, parameter_list):
         pass
 
+    def start(self, parameter_list):
+        pass
+
 class Cliente(object):
+    def __init__(self, nombre : str, orden=[]):
+        pass
+
     def ordenarPlatillo(self, parameter_list):
         pass
+
     def comerPlatillo(self, parameter_list):
+        pass
+    
+    def start(self, parameter_list):
         pass
 
 
-class Mesero(object):
+class Cocinero(object):
+    def __init__(self, nombre : str):
+        pass
+
     def prepararPlatillo(self, parameter_list):
+        pass
+
+    def start(self, parameter_list):
         pass
 
 class Platillo(object):
