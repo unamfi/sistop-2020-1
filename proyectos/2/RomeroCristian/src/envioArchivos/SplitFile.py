1ddec3274b2e361239e5e61d5fa406f2dfb9d5d1
Cristian Romero <mascrit@gmail.com>
2019-09-30 17:34:12 -0500

    Jugando con los semaforos, Ui Lista

--- proyectos/2/RomeroCristian/src/envioArchivos/SplitFile.py
+++ proyectos/2/RomeroCristian/src/envioArchivos/SplitFile.py
@@ -2,17 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from threading import Thread, Semaphore
-from os import getcwd
+from os import getcwd, remove
+from os.path import basename
 
 class SplitFile:
 
-    def __init__(self, archivo: str, mutex: Semaphore,
-                 num_splits=4, dest_dir=getcwd()):
+    def __init__(self, archivo: str, mutex, num_splits=4, dest_dir=getcwd()):
         self.archivo = open(archivo, "rb").read()
         self.nombre = archivo
         self.num_splits = num_splits
         self.dest_dir = dest_dir
+        print(self.dest_dir)
         self.mutex = mutex
+        self.list_gen = list()
 
     def start_split(self, split_max=4):
         tamanio = len(self.archivo)
@@ -21,23 +23,31 @@ class SplitFile:
             splits.append(tamanio//split_max)
             split_max -= 1
         ini = cont = 0
-        with self.mutex:
-            while splits:
-                popper = splits.pop(0)
-                Thread(target=self.split, args=((ini, popper), cont)).start()
-                ini += 1
-                cont += 1
+        while splits:
+            popper = splits.pop(0)
+            Thread(target=self.split, args=((ini, popper), cont)).start()
+            ini += 1
+            cont += 1
 
     def split(self, rango: tuple, num_split: int):
         """
         :param rango: Recibe el rango de corte del archivo (inicio, fin)
         """
         ext = ".{}.part".format(num_split)
-        archivo_p = open("{}{}".format(self.nombre, ext), "wb+")
-        archivo_p.write(self.archivo[rango[0]:rango[1]])
+        self.list_gen.append("{}{}".format(self.dest_dir+'/.'+basename(self.nombre), ext))
+        archivo_p = open("{}{}".format(self.dest_dir+'/.'+basename(self.nombre), ext), "wb+")
+
+        bina = self.archivo
+        archivo_p.write(bina[rango[0]:rango[1]])
+        print(archivo_p)
+        archivo_p.close()
+
+    def clean_part(self):
+        with self.mutex:
+            [remove(i) for i in self.list_gen]
 
 if __name__=='__main__':
 
     mut = Semaphore(1)
-    sp = SplitFile('wxglade_out.py', mut)
+    sp = SplitFile('UNAM_INGENIERIA-eps-converted-to.pdf', mut)
     sp.start_split()
