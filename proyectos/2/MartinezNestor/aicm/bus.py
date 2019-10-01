4b01c48078f2b583e635be822db9af3456daa771
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-10-01 15:15:31 -0500

    added thread implementation and str override

--- proyectos/2/MartinezNestor/aicm/bus.py
+++ proyectos/2/MartinezNestor/aicm/bus.py
@@ -1,8 +1,17 @@
 from aicm.transport import Transport, TransportType
+from threading import Thread
 
-class Bus(Transport):	
+class Bus(Transport,Thread):	
 	def __init__(self,id):
+		Thread.__init__(self)
 		self.id = id
 		self.transport_type = TransportType.bus
 		super(Bus,self)
+
+	def run(self):
+		print(self)
+
+	def __str__(self):
+		return "\t\tBus #" + str(self.id+1) + " ready"		
+			
 		
