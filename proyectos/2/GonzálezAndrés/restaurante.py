37bebd1952ebc1a7e1320e4425473b7256d0e5a1
Andrés González <gandresto@outlook.com>
2019-10-01 23:11:21 -0500

    Implementación fea

--- proyectos/2/GonzálezAndrés/restaurante.py
+++ proyectos/2/GonzálezAndrés/restaurante.py
@@ -2,6 +2,7 @@
 import threading as th
 from faker import Faker
 import random
+from itertools import count
 from time import sleep
 
 menu = [
@@ -19,25 +20,38 @@ menu = [
     'Tacos dorados de papa',
     'Tacos dorados combinados',
     'Especial de la casa',
+    'Cochinita',
+    'Tinga de res',
+    'Tinga de pollo'
 ]
+
 clientes = []
+meseros = []
 ordenesListas = []
 ordenesSinAtender = []
 ordenesSinCocinar = []
+platillos = []
 
 # Candados, semáforos, apagadores, necesarios para sincronización
 lClientes = th.Lock()
+lMeseros = th.Lock()
 lMenu = th.Lock()
 lOrdenesListas = th.Lock()
 lOrdenesSinAtender = th.Lock()
 lOrdenesSinCocinar = th.Lock()
-apagadorClientes = th.Semaphore(0) # Apagador para que el mesero duerma si no hay clientes
+cvClientes = th.Condition() #Variable de condición para que los meseros esperen si no hay clientes
 semOrdenesSinAtender = th.Semaphore(0) # Semáforo que los clientes levantarán para indicar que hay una orden que atender
 
+def hayClientes():
+    with lClientes:
+        losHay = True if clientes else False
+    return losHay
 
 class Mesero(th.Thread):
+    _ids = count(0)
     def __init__(self, name : str):
         th.Thread.__init__(self)
+        self.id = next(self._ids)
         self.name = name
         self.lock = th.Lock()
 
@@ -54,13 +68,31 @@ class Mesero(th.Thread):
         pass
 
     def run(self):
-        apagadorClientes.acquire()
+        global clientes, meseros
+        print('Hola, soy %s y voy a atender clientes.' % self.name)
+        with lMeseros:
+            meseros.append(self)
+        print('%s: Somos %i meseros en el restaurante.' % (self.name, self._ids))
+        cvClientes.acquire()
+        while not clientes:
+            print("No hay clientes, me wa dormir. Zzzz")
+            cvClientes.wait()
+        with lClientes:
+            clientePorAtender = clientes.pop(0)
+        cvClientes.release()
+        print('%s: atendiendo a cliente: %s' % (self.name, clientePorAtender.name))
+        self.recibirOrden()
+        
 
 class Cliente(th.Thread):
-    def __init__(self, name : str, orden):
+    _ids = count(0)
+    def __init__(self, name, orden):
         th.Thread.__init__(self)
+        self.id = next(self._ids)
         self.name = name
         self.orden = orden
+        self.ordeno = False
+        self.comio = False
         #self.lOrden = th.Lock()
 
     def ordenarPlatillos(self):
@@ -71,30 +103,36 @@ class Cliente(th.Thread):
     def comerPlatillo(self):
         pass
 
-    def run(self):
-        print('Hola, soy %s y voy a entrar al restaurante.' % self.name, end=' ')
-        print('Voy a ordenar %i platillo(s).' % len(self.orden))
-        with lClientes:
+    def entraralRestaurante(self):
+        with cvClientes:
             clientes.append(self)
             print('%s: Estoy dentro, somos %i en el restaurante.' % (self.name, len(clientes)))
+            cvClientes.notify() # Aviso a los meseros que hay clientes
+            
+    def run(self):
+        global clientes, cvClientes
+        print('Hola, soy %s y voy a entrar al restaurante.' % self.name, end=' ')
+        print('Voy a ordenar %i platillo(s).' % len(self.orden))
         
-        apagadorClientes.release() # Aviso que hay un cliente por ser atendido
+        self.entraralRestaurante()
 
         with lOrdenesSinAtender:
             ordenesSinAtender.append(self.orden)
-        semOrdenesSinAtender.release() # Aviso a los meseros que hay órdenes por atender
-        self.orden.mesero.lock.acquire() # Aviso a los meseros que hay órdenes por atender
+            # Aviso a los meseros que hay órdenes por atender
+        
         self.ordenarPlatillos()
 
         self.comerPlatillo()
-
+        sleep(2)
         with lClientes:
             clientes.remove(self)
             print('%s: Terminé de comer, quedan %i en el restaurante.' % (self.name, len(clientes)))
 
 class Cocinero(th.Thread):
+    _ids = count(0)
     def __init__(self, name : str):
         th.Thread.__init__(self)
+        self.id = next(self._ids)
         self.name = name
 
     def prepararPlatillo(self):
@@ -104,18 +142,21 @@ class Cocinero(th.Thread):
         pass
 
 class Orden(object):
-    def __init__(self, id, platillos, cliente, mesero):
-        self.id = id
-        self.platillos = platillos
+    _ids = count(0)
+    def __init__(self, mesero=None):
+        self.id = next(self._ids)
         self.cliente = cliente
         self.mesero = mesero
         self.atendida = False
+        self.cocinada = False
+        self.servida = False
 
 class Platillo(object):
+    _ids = count(0)
     def __init__(self, nombre, orden):
+        self.id = next(self._ids)
         self.nombre = nombre
-        self.orden = orden
-        self.preparado = False
+        self.cocinado = False
     def __str__(self):
         return self.nombre
 
