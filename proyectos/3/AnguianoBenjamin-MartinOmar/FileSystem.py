8109c1a1952aff3aee3e33bf8ed807764d261b0a
omar martin <omarmartinmancilla@outlook.com>
2019-10-21 11:26:31 -0500

    programa base, con sólo el método getFileSystem, el cual obtiene el sistema de archivos y los ficheros que contiene

---
+++ proyectos/3/AnguianoBenjamin-MartinOmar/FileSystem.py
@@ -0,0 +1,28 @@
+
+
+ficheros = []
+tamanio = []
+ubicacion = []
+
+def getFileSystem():
+	fileSystem = open('fiunamfs.img','r')
+	posicion = 2048
+	while posicion < 10240:
+		fileSystem.seek(posicion)
+		consulta = fileSystem.read(15)
+		if(consulta != 'Xx.xXx.xXx.xXx.'):
+			ficheros.append(consulta.replace(" "," "))
+			fileSystem.seek(posicion+16)
+			tamanio.append(fileSystem.read(8))
+			fileSystem.seek(posicion+25)
+			ubicacion.append(fileSystem.read(5))
+			
+		posicion+=64
+	posicion = 10240
+	fileSystem.close()
+	
+
+def main():
+	getFileSystem()
+
+main()
