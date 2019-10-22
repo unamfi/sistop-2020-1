5bc325b65b93f80e2c9fd6397c40d97e9bcd2c13
omar martin <omarmartinmancilla@outlook.com>
2019-10-21 21:54:30 -0500

    añado método infoFileSystem que imprime la información del FileSystem, además modifiqué el método getFileSystem para que devuelva los archivos que encuentra

--- proyectos/3/AnguianoBenjamin-MartinOmar/FileSystem.py
+++ proyectos/3/AnguianoBenjamin-MartinOmar/FileSystem.py
@@ -3,6 +3,7 @@
 ficheros = []
 tamanio = []
 ubicacion = []
+informacion = []
 
 def getFileSystem():
 	fileSystem = open('fiunamfs.img','r')
@@ -15,14 +16,33 @@ def getFileSystem():
 			fileSystem.seek(posicion+16)
 			tamanio.append(fileSystem.read(8))
 			fileSystem.seek(posicion+25)
-			ubicacion.append(fileSystem.read(5))
-			
+			ubicacion.append(fileSystem.read(5))	
 		posicion+=64
-	posicion = 10240
 	fileSystem.close()
-	
+
+	rango = len(ficheros)
+	for i in range(rango):
+		print("nombre: "+ficheros[i]+" tamanio: "+tamanio[i]+" ubicacion: "+ubicacion[i])
+		
+def infoFileSystem():
+	fileSystem = open('fiunamfs.img', 'r')
+
+	anadirInfo(0,fileSystem,8)
+	anadirInfo(10,fileSystem,3)
+	anadirInfo(20,fileSystem,15)
+	anadirInfo(40,fileSystem,5)
+	anadirInfo(47,fileSystem,2)
+	anadirInfo(52,fileSystem,8)
+	print(informacion)
+
+	fileSystem.close()
+
+def anadirInfo(posicion,fileSystem, duracion):
+	fileSystem.seek(posicion)
+	informacion.append(fileSystem.read(duracion))
+
 
 def main():
 	getFileSystem()
-
+	#infoFileSystem()
 main()
