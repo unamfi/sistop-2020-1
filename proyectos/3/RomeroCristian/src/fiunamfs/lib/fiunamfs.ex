3bf96ba19cd588f88c34265e07705c155041e7b3
Cristian Romero Andrade <cristian.romero@comunidad.unam.mx>
2019-10-23 21:16:07 -0500

    Eleccion de elixir por que me gusto el lenguaje, menu listo

---
+++ proyectos/3/RomeroCristian/src/fiunamfs/lib/fiunamfs.ex
@@ -0,0 +1,64 @@
+defmodule Fiunamfs do
+  @moduledoc """
+  Documentation for Fiunamfs.
+  """
+
+  def main(args) do
+    args
+    |> analizar_argumento
+    |> procesar_argumentos
+  end
+
+  defp analizar_argumento(args) do
+    OptionParser.parse(args,
+      switches: [
+	help: :boolean,
+	root: :string,
+	name_vol: :string,
+	create: :boolean],
+      aliases: [
+	h: :help,
+	r: :root,
+	n: :name_vol])
+  end
+
+  defp procesar_argumentos(opts) do
+    case opts do
+      {[help: true],_, _} ->
+	IO.puts(print_help())
+	System.halt(0)
+      _ ->
+	IO.puts(print_help_manual())
+	System.halt(1)
+    end
+  end
+
+  defp print_help do
+    man_root = "Define el archivo que contiene o "<>
+      "contendra el sistema de archivos"
+    man_name_vol = "Define la etiqueta del volumen"
+    man_create = "Define si se creara nuevo sistema de archivos"
+    """
+    
+    $ fiunamfs [COMMANDOS]
+
+    FiunamFS
+
+    Comandos:
+    \t--help, -h\tImprime este manual.
+    \t--root, -r\t#{man_root}
+    \t--name_vol, -v\t#{man_name_vol}
+    \t--create, -c\t#{man_create}
+
+    """
+  end
+
+  defp print_help_manual do
+    """
+    \e[0;31mError fatal\e[0;0m: No se especifica nada
+    --help, -h\tImprime la ayuda
+    """
+  end
+
+  
+end
---
+++ proyectos/3/RomeroCristian/src/fiunamfs/test/fiunamfs_test.exs
@@ -0,0 +1,8 @@
+defmodule FiunamfsTest do
+  use ExUnit.Case
+  doctest Fiunamfs
+
+  test "greets the world" do
+    assert Fiunamfs.hello() == :world
+  end
+end
