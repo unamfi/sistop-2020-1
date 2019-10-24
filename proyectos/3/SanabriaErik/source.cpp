ecaee36a76c21c8f894e229febe14b90e70b7cdf
Erik Sanabria <esanabria136@gmail.com>
2019-10-24 16:23:02 -0500

    creando proyecto

---
+++ proyectos/3/SanabriaErik/source.cpp
@@ -0,0 +1,18 @@
+/*
+ * source.cpp
+ *
+ *  Created on: Oct 24, 2019
+ *      Author: erik
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+
+	std::cout << "\n\n";
+
+	return 0;
+}
+
+
