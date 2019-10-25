618a68aa469a1573b0bc8c5ce11479464f4bf366
Erik Sanabria <esanabria136@gmail.com>
2019-10-25 08:31:17 -0500

    agregando img

--- proyectos/3/SanabriaErik/source.cpp
+++ proyectos/3/SanabriaErik/source.cpp
@@ -6,11 +6,36 @@
  */
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
 int main(int argc, char* argv[])
 {
+	std::string fname{ "res/fiunamfs.img" };
+	std::ifstream in_f;
 
-	std::cout << "\n\n";
+	const char *mag_def = "FiUnamFS";
+
+	if(argc > 1)
+	{
+		fname = *argv[1];
+	}
+
+	in_f.open(fname, std::ios::binary | std::ios::in);
+
+	char mag[8];
+
+	in_f.read(mag, 8);
+
+	if(mag != mag_def)
+	{
+		std::cout << std::endl << "El archivo no es valido..." << mag << std::endl;
+		return 1;
+	}
+
+	std::cout << std::endl << "Data:\t" << mag << std::endl;
+
+	in_f.close();
 
 	return 0;
 }
