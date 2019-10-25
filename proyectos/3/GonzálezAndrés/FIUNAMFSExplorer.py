6463d55f33b2e5fa70f4d436bb44c6d4779b1634
Andrés González <gandresto@outlook.com>
2019-10-25 12:46:22 -0500

    Scandir para obtener info completa de las entradas del directorio

--- proyectos/3/GonzálezAndrés/FIUNAMFSExplorer.py
+++ proyectos/3/GonzálezAndrés/FIUNAMFSExplorer.py
@@ -8,5 +8,9 @@ fs = FIUNAMFS(fsimg_path)
 fs.montar()
 l_archivos = fs.listdir()
 print(l_archivos)
+lEntDir = fs.scandir()
+for entDir in lEntDir:
+    print('%i %s %i bytes %s %s' %
+            (entDir.tam_archivo, entDir.nombre, entDir.cluster_inicial, entDir.f_creacion, entDir.f_modif))
 fs.desmontar()
 fs.desmontar()
--- proyectos/3/GonzálezAndrés/fiunamfs.py
+++ proyectos/3/GonzálezAndrés/fiunamfs.py
@@ -3,6 +3,7 @@ from mmap import mmap
 class FIUNAMFS(object):
     def __init__(self, ruta_img):
         self.ruta_img = ruta_img
+        self.__listaEntDir = []
         self.montado = False
 
     def montar(self):
@@ -39,11 +40,15 @@ class FIUNAMFS(object):
 
                 self.tam_entradadir = 64
 
+                self.scandir()
+
                 print('Sistema de archivos montado')
             else:
                 print('No se reconoce el sistema de archivos')
                 self.__mmfs.close()
                 self.montado = False
+
+    
 
     def desmontar(self):
         if self.montado:
@@ -52,37 +57,51 @@ class FIUNAMFS(object):
             print('Sistema de archivos desmontado')
         else:
             print('No se ha montado el sistema de archivos')
-    
+
     def listdir(self):
         ldir = []
+        if self.montado:
+            # inicio = self.tam_cluster
+            # fin = self.tam_cluster*self.tam_dir+self.tam_cluster
+            # paso = self.tam_entradadir
+            # # cluster_actual = 1
+            # for i in range(inicio,fin,paso):
+            #     entdir = self.__mmfs[i:i+paso]
+            #     nombre = entdir[0:15].decode('ascii').strip()
+            #     if nombre != 'Xx.xXx.xXx.xXx.':
+            #         #print(nombre)
+            #         ldir.append(nombre)
+            for entradaDir in self.__listaEntDir:
+                ldir.append(entradaDir.nombre)
+        else: 
+            print('No se ha montado el sistema de archivos')
+        return ldir
+    
+    def scandir(self):
+        self.__listaEntDir = []
         if self.montado:
             inicio = self.tam_cluster
             fin = self.tam_cluster*self.tam_dir+self.tam_cluster
             paso = self.tam_entradadir
-            # cluster_actual = 1
             for i in range(inicio,fin,paso):
                 entdir = self.__mmfs[i:i+paso]
                 nombre = entdir[0:15].decode('ascii').strip()
-
-                # if (i % self.tam_cluster) == 0:
-                #     print('Cluster actual: %i' % cluster_actual)
-                #     cluster_actual+=1
-
-                # if nombre == '\0'*15:
-                #     cadena = 'Xx.xXx.xXx.xXx.'.encode('ascii')
-                #     print(len(cadena), cadena)
-                #     print(len(self.__mmfs[i:i+15]), self.__mmfs[i:i+paso])
-                #     try:
-                #         self.__mmfs[i:i+15] = cadena
-                #     except IndexError as ierr:
-                #         print('indexerror')
-                    
                 if nombre != 'Xx.xXx.xXx.xXx.':
-                    #print(nombre)
-                    ldir.append(nombre)
-        else: 
+                    tam_archivo = int(entdir[16:24].decode('ascii').strip())
+                    cluster_inicial = int(entdir[25:30].decode('ascii').strip())
+                    f_creacion = entdir[31:45].decode('ascii')
+                    f_modif = entdir[46:60].decode('ascii')
+                    nuevaEntrada = EntradaDir(nombre, tam_archivo, cluster_inicial, f_creacion, f_modif)
+                    self.__listaEntDir.append(nuevaEntrada)
+        else:
             print('No se ha montado el sistema de archivos')
-        return ldir
+        return self.__listaEntDir
+
+
 class EntradaDir(object):
-    def __init__(self, nombre_arch, tam_archivo, cluster_i, f_creacion, f_modif):
-        pass
+    def __init__(self, nombre, tam_archivo, cluster_inicial, f_creacion, f_modif):
+        self.nombre = nombre
+        self.tam_archivo = tam_archivo
+        self.cluster_inicial = cluster_inicial
+        self.f_creacion = f_creacion
+        self.f_modif = f_modif
