1ac13088083eabf3eaaeb997cd6ee89018bcb7bb
Andrés González <gandresto@outlook.com>
2019-10-25 15:08:17 -0500

    Función para descargar archivos a un fs local.

--- proyectos/3/GonzálezAndrés/fiunamfs.py
+++ proyectos/3/GonzálezAndrés/fiunamfs.py
@@ -1,5 +1,9 @@
 from mmap import mmap
 
+MSGERR_NO_MONTADO = 'Error: No se ha montado el sistema de archivos'
+MSGERR_ARCH_NO_ENC = 'Error: No se encuentra el archivo de origen'
+MSGADV_FS_YA_MONT = 'Advertencia: El sistema de archivos ya está montado'
+
 class FIUNAMFS(object):
     def __init__(self, ruta_img):
         self.ruta_img = ruta_img
@@ -7,48 +11,52 @@ class FIUNAMFS(object):
         self.montado = False
 
     def montar(self):
-        try:
-            f = open(self.ruta_img, 'r+b')
-        except OSError:
-            print('No se puede abrir %s' % self.ruta_img)
-        except IOError as e:
-            print('IOError %s' % e)
-        else:
-            self.__mmfs = mmap(f.fileno(), 0)
-            nombrefs = self.__mmfs[0:8].decode('ascii')
-            # print(nombrefs)
-            if nombrefs == 'FiUnamFS':
-                self.montado = True
-
-                self.nombre = nombrefs
-                print('Nombre del sistema de archivos: %s' % self.nombre)
-
-                self.version = self.__mmfs[10:13].decode('ascii').strip()
-                print('Versión: %s' % self.version)
-                
-                self.etiqueta = self.__mmfs[20:35].decode('ascii').strip()
-                print('Etiqueta del volumen: %s' % self.etiqueta)
-                
-                self.tam_cluster = int(self.__mmfs[40:45].decode('ascii').strip())
-                print('Tamaño del cluster: %i bytes' % self.tam_cluster)
-
-                self.tam_dir = int(self.__mmfs[47:49].decode('ascii').strip())
-                print('Tamaño del directorio: %i clusters' % self.tam_dir)
-
-                self.tam_unidad = int(self.__mmfs[52:60].decode('ascii').strip())
-                print('Tamaño de la unidad: %i clusters' % self.tam_unidad)
-
-                self.tam_entradadir = 64
-
-                self.scandir()
-
-                print('Sistema de archivos montado')
+        if not self.montado:                
+            try:
+                f = open(self.ruta_img, 'r+b')
+            except OSError:
+                print('No se puede abrir %s' % self.ruta_img)
+            except IOError as e:
+                print('IOError %s' % e)
             else:
-                print('No se reconoce el sistema de archivos')
-                self.__mmfs.close()
-                self.montado = False
+                self.__mmfs = mmap(f.fileno(), 0)
+                nombrefs = self.__mmfs[0:8].decode('ascii')
+                # print(nombrefs)
+                if nombrefs == 'FiUnamFS':
+                    self.montado = True
 
-    
+                    self.nombre = nombrefs
+                    # print('Nombre del sistema de archivos: %s' % self.nombre)
+
+                    self.version = self.__mmfs[10:13].decode('ascii').strip()
+                    # print('Versión: %s' % self.version)
+                    
+                    self.etiqueta = self.__mmfs[20:35].decode('ascii').strip()
+                    # print('Etiqueta del volumen: %s' % self.etiqueta)
+                    
+                    self.tam_cluster = int(self.__mmfs[40:45].decode('ascii').strip())
+                    # print('Tamaño del cluster: %i bytes' % self.tam_cluster)
+
+                    self.tam_dir = int(self.__mmfs[47:49].decode('ascii').strip())
+                    # print('Tamaño del directorio: %i clusters' % self.tam_dir)
+
+                    self.tam_unidad = int(self.__mmfs[52:60].decode('ascii').strip())
+                    # print('Tamaño de la unidad: %i clusters' % self.tam_unidad)
+
+                    self.tam_entradadir = 64
+
+                    self.scandir()
+
+                    print('Sistema de archivos montado')
+                    return True
+                else:
+                    print('No se reconoce el sistema de archivos')
+                    self.__mmfs.close()
+                    self.montado = False
+                    return False
+        else:
+            print(MSGADV_FS_YA_MONT)
+            return True
 
     def desmontar(self):
         if self.montado:
@@ -56,25 +64,15 @@ class FIUNAMFS(object):
             self.montado = False
             print('Sistema de archivos desmontado')
         else:
-            print('No se ha montado el sistema de archivos')
+            print(MSGERR_NO_MONTADO)
 
     def listdir(self):
         ldir = []
         if self.montado:
-            # inicio = self.tam_cluster
-            # fin = self.tam_cluster*self.tam_dir+self.tam_cluster
-            # paso = self.tam_entradadir
-            # # cluster_actual = 1
-            # for i in range(inicio,fin,paso):
-            #     entdir = self.__mmfs[i:i+paso]
-            #     nombre = entdir[0:15].decode('ascii').strip()
-            #     if nombre != 'Xx.xXx.xXx.xXx.':
-            #         #print(nombre)
-            #         ldir.append(nombre)
             for entradaDir in self.__listaEntDir:
                 ldir.append(entradaDir.nombre)
         else: 
-            print('No se ha montado el sistema de archivos')
+            print(MSGERR_NO_MONTADO)
         return ldir
     
     def scandir(self):
@@ -94,9 +92,36 @@ class FIUNAMFS(object):
                     nuevaEntrada = EntradaDir(nombre, tam_archivo, cluster_inicial, f_creacion, f_modif)
                     self.__listaEntDir.append(nuevaEntrada)
         else:
-            print('No se ha montado el sistema de archivos')
+            print(MSGERR_NO_MONTADO)
         return self.__listaEntDir
 
+    def descargar(self, origen = '', destino = ''):
+        """Copiar un archivo de FIUNAMFS a nuestro sistema de archivos
+        Atributos:
+            origen -- nombre del archivo a copiar
+            destino -- ruta y nombre del archivo de destino
+        """
+        if self.montado:
+            resultado = list(filter( lambda entdir: entdir.nombre == origen, self.__listaEntDir)) # Buscamos el elemento que coincida
+            if resultado:
+                entrDir = resultado.pop() # Obtenemos la entrada del directorio
+                dir_inicio = entrDir.cluster_inicial * self.tam_cluster # dirección de inicio
+                dir_fin = dir_inicio + entrDir.tam_archivo # dirección de fin
+                bytes_archivo = self.__mmfs[dir_inicio:dir_fin] # bytes en bruto
+                try:
+                    destino = origen if not destino else destino # Usar el mismo nombre que el del origen si no le dimos ninguno
+                    f = open(destino, 'w+b')
+                    f.write(bytes_archivo)
+                    f.close()
+                    print('Se escribió el archivo %s' % destino)
+                    return True
+                except IOError as ioerr:
+                    print('IOError: %s' % ioerr)
+            else:
+                print(MSGERR_ARCH_NO_ENC)
+        else:
+            print(MSGERR_NO_MONTADO)
+        return False
 
 class EntradaDir(object):
     def __init__(self, nombre, tam_archivo, cluster_inicial, f_creacion, f_modif):
@@ -105,3 +130,16 @@ class EntradaDir(object):
         self.cluster_inicial = cluster_inicial
         self.f_creacion = f_creacion
         self.f_modif = f_modif
+
+    def __str__(self):
+        return '%i %s %i bytes %s %s' % (self.tam_archivo, 
+                                    self.nombre, 
+                                    self.cluster_inicial, 
+                                    self.f_creacion, 
+                                    self.f_modif)
+
+# Error de sistema no montado
+# class NMError(Error):
+#     def __init__(self, expression, message):
+#         self.expression = expression
+#         self.message = message
