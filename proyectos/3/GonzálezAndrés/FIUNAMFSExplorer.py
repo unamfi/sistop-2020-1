871244ec70f91105ac76a4313635fffdc5255c3c
Andrés González <gandresto@outlook.com>
2019-10-26 11:32:51 -0500

    Probando GUI en v0.7 del fiunamfs

--- proyectos/3/GonzálezAndrés/FIUNAMFSExplorer.py
+++ proyectos/3/GonzálezAndrés/FIUNAMFSExplorer.py
@@ -1,40 +1,78 @@
-# -*- coding: utf-8 -*-
+import tkinter as tk
+from tkinter import ttk
+
 import os
 from fiunamfs import FIUNAMFS
-from tabulate import tabulate
 
-fsimg_path = os.path.join('.', 'fiunamfs.img')
+class FIUNAMFSExplorer:
+    def __init__(self, window=None):
+        self.window = window
+        self.window.title('FIUNAM Explorer')
+        self.create_widgets()
+        # fsimg_path = os.path.join('.', 'fiunamf v0.7.img')
+        # fs = FIUNAMFS(fsimg_path)
+        # fs.montar()
+        # fs.montar()
 
-fs = FIUNAMFS(fsimg_path)
-fs.montar()
-fs.montar()
+        # l_archivos = fs.listdir()
+        # print(l_archivos)
 
-l_archivos = fs.listdir()
-print(l_archivos)
+        # lEntDir = fs.scandir()
 
-lEntDir = fs.scandir()
-encabezados = ['Cluster\nde inicio', 'Nombre', 'Tamaño', 'Fecha de creación', 'Fecha de modificación']
-tabla = []
-for entDir in lEntDir:
-    tabla.append([
-        entDir.cluster_inicial, 
-        entDir.nombre, 
-        '%i bytes' % entDir.tam_archivo, 
-        entDir.f_creacion, 
-        entDir.f_modif
-    ])
-print(tabulate(tabla, encabezados))
+    def create_widgets(self):
+        # Frame Container
+        frame = tk.LabelFrame(self.window, text = 'Agregar archivos')
+        frame.grid(row=0, column=0, columnspan=3, pady=20)
 
-# fs.descargar('README.org', os.path.join('ArchivosDescargados', 'README.org'))
-# fs.descargar('logo.png', os.path.join('ArchivosDescargados', 'logo.png'))
-# fs.descargar('mensajes.png', os.path.join('ArchivosDescargados', 'mensajes.png'))
+        # Name Input
+        tk.Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        self.name = tk.Entry(frame)
+        self.name.focus()
+        self.name.grid(row = 1, column = 1)
 
-fs.subir('README.org', 'OtroMas')
-fs.subir('README.org', 'OtroMas\na')
-fs.subir('README.org', '')
-fs.subir('README.org', 'OtroMás')
-fs.subir('README.org', 'Otro Más  ')
-fs.subir('README.org', 'Otro mass largo........  ')
+        # Price Input
+        tk.Label(frame, text = 'Price: ').grid(row = 2, column = 0)
+        self.price = tk.Entry(frame)
+        self.price.grid(row = 2, column = 1)
 
-fs.desmontar()
-fs.desmontar()
+        # Table
+        self.tree = ttk.Treeview(height = 10, columns = ('#0', '#1', '#2', '#3', '#4'))
+        self.tree.grid(row = 4, column = 0, columnspan = 2)
+        self.tree.heading('#0', text = 'Cluster', anchor = tk.CENTER)
+        self.tree.heading('#1', text = 'Nombre', anchor = tk.CENTER)
+        self.tree.heading('#2', text = 'Tamaño (bytes)', anchor = tk.CENTER)
+        self.tree.heading('#3', text = 'Fecha Creación', anchor = tk.CENTER)
+        self.tree.heading('#4', text = 'Fecha Modificación', anchor = tk.CENTER)
+
+        # self.hi_there = tk.Button(self)
+        # self.hi_there["text"] = "Hello World\n(click me)"
+        # self.hi_there["command"] = self.say_hi
+        # self.hi_there.pack(side="top")
+
+        # self.quit = tk.Button(self, text="QUIT", fg="red",
+        #                       command=self.master.destroy)
+        # self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+    
+    def leer_dir(self):
+        pass
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = FIUNAMFSExplorer(window=root)
+    app.window.mainloop()
+
+# class FIUNAMFSExplorer(object):
+#     def __init__(self, window : tk.Tk):
+#         self.window = window
+#         self.window.title('FIUNAM Explorer')
+#         self.window.geometry((500,600))
+#         frame = tk.LabelFrame(self.window, text='Agregar un archivo')
+#         frame.grid(row=0, column=0, columnspan=3, pady=20)
+
+# if __name__ == "__main__":
+#     window = tk.Tk()
+#     app = FIUNAMFSExplorer(window)
+#     window.mainloop()
---
+++ proyectos/3/GonzálezAndrés/test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import os
+from fiunamfs import FIUNAMFS
+from tabulate import tabulate
+
+fsimg_path = os.path.join('.', 'fiunamfs2.img')
+
+fs = FIUNAMFS(fsimg_path)
+fs.montar()
+fs.montar()
+
+l_archivos = fs.listdir()
+print(l_archivos)
+
+lEntDir = fs.scandir()
+encabezados = ['Cluster\nde inicio', 'Nombre', 'Tamaño', 'Fecha de creación', 'Fecha de modificación']
+tabla = []
+for entDir in lEntDir:
+    tabla.append([
+        entDir.cluster_inicial, 
+        entDir.nombre, 
+        '%i bytes' % entDir.tam_archivo, 
+        entDir.f_creacion, 
+        entDir.f_modif
+    ])
+print(tabulate(tabla, encabezados))
+
+fs.descargar('README.org', os.path.join('ArchivosDescargados', 'README.org'))
+# fs.descargar('logo.png', os.path.join('ArchivosDescargados', 'logo.png'))
+# fs.descargar('mensajes.png', os.path.join('ArchivosDescargados', 'mensajes.png'))
+
+# fs.subir('README.org', 'OtroMas')
+# fs.subir('README.org', 'OtroMas\na')
+fs.subir(os.path.join('ArchivosParaSubir', 'AB_blackbird.jpg'), 'AB_blackbird.jpg')
+fs.subir(os.path.join('ArchivosDescargados', 'README.org'), 'README.org')
+# fs.subir('README.org', 'OtroMás')
+# fs.subir('README.org', 'Otro Más  ')
+# fs.subir('README.org', 'Otro mass largo........  ')
+# fs.subir('README.org', 'a'*16)
+# fs.subir('README.org', 'b'*17)
+
+fs.desmontar()
+fs.desmontar()
