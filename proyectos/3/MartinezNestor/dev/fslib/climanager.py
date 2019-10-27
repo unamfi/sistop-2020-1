58bf7a4f1a50e286865c1fca8b407334700b80d2
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-10-27 14:35:46 -0600

    added support clases for file management

--- proyectos/3/MartinezNestor/dev/fslib/climanager.py
+++ proyectos/3/MartinezNestor/dev/fslib/climanager.py
@@ -1,8 +1,14 @@
+from fslib.filemanager import FileManager
 
 class CommandManager():
 	
+	def __init__(self):
+		f_m = FileManager()
+		self.super_block = f_m.build_sb()
+
 	def ls(self):
 		print("List")
+		print(self.super_block.name)
 
 	def cpi(self, file):
 		print("Copy inside", file)
