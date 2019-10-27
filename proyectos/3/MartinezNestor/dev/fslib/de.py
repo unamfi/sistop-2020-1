8978e2b43d3a59d9f90354c39e0bc7677f18d895
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-10-27 16:18:07 -0600

    added last parameter and changed vars to snake case

--- proyectos/3/MartinezNestor/dev/fslib/de.py
+++ proyectos/3/MartinezNestor/dev/fslib/de.py
@@ -1,9 +1,9 @@
 class DirectoryEntry():
 	"""docstring for Dir"""
-	def __init__(self, filename, filesize, cluster, creation, last_modif):
-		self.filename = filename
-		self.filesize = filesize
+	def __init__(self, file_name, file_size, cluster, creation, last_modif, non_used_space):
+		self.file_name = file_name
+		self.file_size = file_size
 		self.cluster = cluster
 		self.creation = creation
 		self.last_modif = last_modif
-		
+		self.non_used_space = non_used_space
