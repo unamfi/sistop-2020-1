f3e8c32a28a308d2608e38529db71880cfb633c7
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-10-27 18:10:52 -0600

    upgraded classes for listing function

--- proyectos/3/MartinezNestor/dev/fslib/clim.py
+++ proyectos/3/MartinezNestor/dev/fslib/clim.py
@@ -3,16 +3,34 @@ from fslib.de import DirectoryEntry
 
 class CommandManager():
 
+	f_m = FileManager()
+
+	sector_size = 512
+	cluster_size = 512 * 4
+
 	root_dir = []
+	root_dir_clusters = 4
+	root_dir_entry_size = 64
+	root_dir_empty_entry = "Xx.xXx.xXx.xXx."
 	
 	def __init__(self):
-		f_m = FileManager()
-		self.super_block = f_m.build_sb()
-		self.fs_data = f_m.get_fs_data()
+		self.super_block = self.f_m.build_sb()
+		self.file_system = self.f_m.get_fs()
 
 	def ls(self):
-		print("List")
-
+		for cluster in range(self.root_dir_clusters):
+			_c = 0
+			for entry in range(0,self.cluster_size,64):
+				_a = ((cluster+1)*self.cluster_size) + (_c*64)
+				_b = _a + 64
+				_c += 1
+				data = self.file_system[_a:_b]
+				dir_entry = self.f_m.get_de(data)
+				if dir_entry.name == self.root_dir_empty_entry.encode():
+					pass
+				else:
+					self.root_dir.append(dir_entry)
+		self.__print__root(dir=self.root_dir)
 
 	def cpi(self, file):
 		print("Copy inside", file)
@@ -25,5 +43,7 @@ class CommandManager():
 
 	def defrag(self):
 		print("defrag")
-	
-		
+
+	def __print__root(self, dir):
+		for file in dir:
+			print(file.name.decode(), end='\t')
