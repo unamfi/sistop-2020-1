8aa989ff88487bcf6cb9df11dd1e8aa1e64dc98f
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-10-27 19:08:23 -0600

    refactored get_dirs_entries function

--- proyectos/3/MartinezNestor/dev/fslib/clim.py
+++ proyectos/3/MartinezNestor/dev/fslib/clim.py
@@ -12,28 +12,21 @@ class CommandManager():
 	root_dir_clusters = 4
 	root_dir_entry_size = 64
 	root_dir_empty_entry = "Xx.xXx.xXx.xXx."
+
+	available_dir_entries = []
 	
 	def __init__(self):
 		self.super_block = self.f_m.build_sb()
 		self.file_system = self.f_m.get_fs()
+		self.available_dir_entries = self.get_dir_entries(available=True)
 
 	def ls(self):
-		for cluster in range(self.root_dir_clusters):
-			_c = 0
-			for entry in range(0,self.cluster_size,64):
-				_a = ((cluster+1)*self.cluster_size) + (_c*64)
-				_b = _a + 64
-				_c += 1
-				data = self.file_system[_a:_b]
-				dir_entry = self.f_m.get_de(data)
-				if dir_entry.name == self.root_dir_empty_entry.encode():
-					pass
-				else:
-					self.root_dir.append(dir_entry)
+		self.root_dir = self.get_dir_entries()
 		self.__print__root(dir=self.root_dir)
 
 	def cpi(self, file):
-		print("Copy inside", file)
+		data = open(file,'rb').read()
+		dir_entry_id = self.available_dir_entries.pop(0)
 
 	def cpo(self, file):
 		print("Copy outside", file)
@@ -44,6 +37,26 @@ class CommandManager():
 	def defrag(self):
 		print("defrag")
 
+	def get_dir_entries(self, available=False):
+		dir_entries = []
+		av_dir_entries = []
+		for cluster in range(self.root_dir_clusters):
+			_c = 0
+			for entry in range(0,self.cluster_size,64):
+				_a = ((cluster+1)*self.cluster_size) + (_c*64)
+				_b = _a + 64
+				_c += 1
+				dir_entry_data = self.file_system[_a:_b]
+				dir_entry = self.f_m.get_de(data=dir_entry_data, dir_entry_id=_c)
+				if dir_entry.name == self.root_dir_empty_entry.encode():
+					av_dir_entries.append(_c)
+				else:
+					dir_entries.append(dir_entry)
+		if available:
+			return av_dir_entries
+		else:
+			return dir_entries
+
 	def __print__root(self, dir):
 		for file in dir:
-			print(file.name.decode())
+			print("%s" %(file.name.decode()))
