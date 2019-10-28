ddbf39ff1d4ae76443abe133de088c93ee1fde32
Andrés González <gandresto@outlook.com>
2019-10-27 21:10:00 -0600

    Función para mostrar tabla bonita

--- proyectos/3/GonzálezAndrés/test.py
+++ proyectos/3/GonzálezAndrés/test.py
@@ -3,6 +3,19 @@ import os
 from fiunamfs import FIUNAMFS, format_date
 from tabulate import tabulate
 
+def muestraTablaDir(listaEntDir):
+    encabezados = ['Cluster\nde inicio', 'Nombre', 'Tamaño', 'Fecha de creación', 'Fecha de modificación']
+    tabla = []
+    for entDir in listaEntDir:
+        tabla.append([
+            entDir.cluster_inicial, 
+            entDir.nombre, 
+            '%i bytes' % entDir.tam_archivo, 
+            format_date(entDir.f_creacion), 
+            format_date(entDir.f_modif)
+        ])
+    print('\n', tabulate(tabla, encabezados), '\n')
+
 fsimg_path = os.path.join('.', 'fiunamfs v0.8.img')
 
 fs = FIUNAMFS(fsimg_path)
@@ -12,28 +25,19 @@ fs.montar()
 l_archivos = fs.listdir()
 print(l_archivos)
 
-lEntDir = fs.scandir()
-encabezados = ['Cluster\nde inicio', 'Nombre', 'Tamaño', 'Fecha de creación', 'Fecha de modificación']
-tabla = []
-for entDir in lEntDir:
-    tabla.append([
-        entDir.cluster_inicial, 
-        entDir.nombre, 
-        '%i bytes' % entDir.tam_archivo, 
-        format_date(entDir.f_creacion), 
-        format_date(entDir.f_modif)
-    ])
-print('\n', tabulate(tabla, encabezados), '\n')
+muestraTablaDir(fs.scandir())
+
 
 fs.descargar('README.org', os.path.join('ArchivosDescargados', 'README.org'))
 fs.descargar('logo.png', os.path.join('ArchivosDescargados', 'logo.png'))
 fs.descargar('mensajes.png', os.path.join('ArchivosDescargados', 'mensajes.png'))
 # fs.descargar('blackbird.jpg', os.path.join('ArchivosDescargados', 'blackbird.jpg'))
 
+fs.subir(os.path.join('ArchivosParaSubir', 'blackbird.jpg'), 'blackbird.jpg')
+muestraTablaDir(fs.scandir())
+
 # fs.subir('README.org', 'OtroMas')
 # fs.subir('README.org', 'OtroMas\na')
-# fs.subir(os.path.join('ArchivosParaSubir', 'blackbird.jpg'), 'blackbird.jpg')
-
 # fs.subir(os.path.join('ArchivosDescargados', 'README.org'), 'README.org')
 # fs.subir('README.org', 'OtroMás')
 # fs.subir('README.org', 'Otro Más  ')
